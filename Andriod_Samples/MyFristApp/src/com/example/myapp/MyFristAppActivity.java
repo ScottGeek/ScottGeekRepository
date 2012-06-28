f2f870ee66dff4548970c490dfa85a494e34da8b
Scott Edwards <cse.edwards@gmail.com>
2012-06-28 19:43:54 -0400

    Andriod Samples Area

---
+++ Andriod_Samples/MyFristApp/src/com/example/myapp/MyFristAppActivity.java
@@ -0,0 +1,13 @@
+package com.example.myapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyFristAppActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
